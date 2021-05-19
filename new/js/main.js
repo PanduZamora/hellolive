32b9b8f2abd4c884a86817ed5eed876f559fef9e
Rahmat Syaefullah <31819246+rahmatsya@users.noreply.github.com>
2021-05-19 14:26:53 +0700

    update new site

--- new/js/main.js
+++ new/js/main.js
@@ -1,9 +1,9 @@
 const BODY = document.getElementsByTagName('body')[0];
 const APP_NAVBAR = document.getElementById('mainNavbar');
-BODY.onscroll = function(){
-    if(window.scrollY > 80){
-        APP_NAVBAR.classList.add('navbar-scrolled');
-    }else{
-        APP_NAVBAR.classList.remove('navbar-scrolled');
-    }
-}
+// BODY.onscroll = function(){
+//     if(window.scrollY > 80){
+//         APP_NAVBAR.classList.add('navbar-scrolled');
+//     }else{
+//         APP_NAVBAR.classList.remove('navbar-scrolled');
+//     }
+// }
